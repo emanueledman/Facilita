047b2b15763e24a708a3703d89f371635b8ede90
Emanuel Joao <emanuelhedima@gmail.com>
2025-04-18 23:48:48 +0100

    Primeira submissao

--- fila_online/serializers.py
+++ fila_online/serializers.py
@@ -1,29 +1,25 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated
-from .models import Instituicao, Filial, Categoria
-from .serializers import InstituicaoSerializer, FilialSerializer, CategoriaSerializer
+from rest_framework import serializers
+from .models import Fila, Ticket, Departamento
+from sistema.serializers import FilialSerializer, CategoriaSerializer
 
-class ListarInstituicoes(APIView):
-    permission_classes = [IsAuthenticated]
+class DepartamentoSerializer(serializers.ModelSerializer):
+    filial = FilialSerializer(read_only=True)
 
-    def get(self, request):
-        instituicoes = Instituicao.objects.all()
-        serializer = InstituicaoSerializer(instituicoes, many=True)
-        return Response(serializer.data)
+    class Meta:
+        model = Departamento
+        fields = ['id', 'filial', 'nome', 'setor']
 
-class ListarFiliais(APIView):
-    permission_classes = [IsAuthenticated]
+class FilaSerializer(serializers.ModelSerializer):
+    departamento = DepartamentoSerializer(read_only=True)
+    categoria = CategoriaSerializer(read_only=True)
 
-    def get(self, request):
-        filiais = Filial.objects.all()
-        serializer = FilialSerializer(filiais, many=True)
-        return Response(serializer.data)
+    class Meta:
+        model = Fila
+        fields = ['id', 'departamento', 'servico', 'categoria', 'prefixo', 'hora_abertura', 'hora_fechamento', 'limite_diario', 'tickets_ativos', 'ticket_atual', 'tempo_espera_medio', 'num_balcoes']
 
-class ListarCategorias(APIView):
-    permission_classes = [IsAuthenticated]
+class TicketSerializer(serializers.ModelSerializer):
+    fila = FilaSerializer(read_only=True)
 
-    def get(self, request):
-        categorias = Categoria.objects.all()
-        serializer = CategoriaSerializer(categorias, many=True)
-        return Response(serializer.data)
+    class Meta:
+        model = Ticket
+        fields = ['id', 'fila', 'numero_ticket', 'codigo_qr', 'prioridade', 'e_fisico', 'status', 'emitido_em', 'expira_em', 'atendido_em', 'balcao']
